53b189ce15ddcb9176f685dcf085c084076520df
CedrickMansoni <cedrick.k.mansoni@gmail.com>
2025-03-14 13:18:54 +0100

    Reajuste do arquivo program para habilitar o swagger

--- Padaria.WebApi/Program.cs
+++ Padaria.WebApi/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using Padaria.WebApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,13 +11,64 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+string conexao = string.Empty;
+# if DEBUG
+    conexao = builder.Configuration.GetConnectionString("LocalConnection")!;
+#else 
+    conexao = builder.Configuration.GetConnectionString("ProdutionConnection")!;
+#endif
+
+builder.Services.AddDbContext<AppDataContext>(options => options.UseNpgsql(conexao));
+
+// Adiciona CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()  // Permite qualquer origem
+              .AllowAnyHeader()  // Permite qualquer cabeçalho
+              .AllowAnyMethod(); // Permite qualquer método HTTP
+    });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json","PADARIA_APP"));
 }
+
+string storagePath = app.Environment.IsDevelopment() ?
+"/home/ckm/Storage/Padaria" : 
+"/home/GSA_PROJECT/Storage/Padaria";
+
+// Configurar middleware para servir arquivos de um diretório externo
+app.UseStaticFiles(new StaticFileOptions
+{
+    //--- 
+    FileProvider = new PhysicalFileProvider(storagePath),
+    RequestPath = "/images", // Caminho acessível via URL
+    ServeUnknownFileTypes = false, // Não servir tipos desconhecidos
+    DefaultContentType = "image/jpeg", // Conteúdo padrão caso a extensão não seja reconhecida
+    OnPrepareResponse = ctx =>
+    {
+        // Bloquear acesso a arquivos sem extensões de imagem
+        if (!ctx.File.Name.EndsWith(".jpg", StringComparison.OrdinalIgnoreCase) &&
+            !ctx.File.Name.EndsWith(".png", StringComparison.OrdinalIgnoreCase) &&
+            !ctx.File.Name.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase) &&
+            !ctx.File.Name.EndsWith(".doc", StringComparison.OrdinalIgnoreCase) &&
+            !ctx.File.Name.EndsWith(".docx", StringComparison.OrdinalIgnoreCase) &&
+            !ctx.File.Name.EndsWith(".xls", StringComparison.OrdinalIgnoreCase) &&
+            !ctx.File.Name.EndsWith(".xlsx", StringComparison.OrdinalIgnoreCase))
+        {
+            ctx.Context.Response.StatusCode = StatusCodes.Status403Forbidden;
+        }
+    }
+});
+
 
 app.UseHttpsRedirection();
 
