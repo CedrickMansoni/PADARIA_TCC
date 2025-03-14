750f261f0c29d57deb3b7de52c8c7d6dae4f5012
CedrickMansoni <cedrick.k.mansoni@gmail.com>
2025-03-14 21:51:56 +0100

    Configuração dos DbSet na classe de contexto

--- Padaria.WebApi/Data/AppDataContext.cs
+++ Padaria.WebApi/Data/AppDataContext.cs
@@ -1,9 +1,19 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using Padaria.WebApi.Models;
 
 namespace Padaria.WebApi.Data;
 
 public class AppDataContext(DbContextOptions<AppDataContext> options) : DbContext(options)
 {
-
+    public DbSet<CategoriaFuncionarioModel> TabelaCategoriaFuncionarioModel { get; set; }
+        public DbSet<FuncionarioModel> TabelaFuncionarioModel { get; set; }
+        public DbSet<ClienteModel> TabelaClienteModel { get; set; }
+        public DbSet<TelefoneModel> TabelaTelefoneModel { get; set; }
+        public DbSet<CategoriaProdutoModel> TabelaCategoriaProdutoModel { get; set; }
+        public DbSet<ProdutoModel> TabelaProdutoModel { get; set; }
+        public DbSet<CaixaModel> TabelaCaixaModel { get; set; }
+        public DbSet<FacturaModel> TabelaFacturaModel { get; set; }
+        public DbSet<EncomendaModel> TabelaEncomendaModel { get; set; }
+        public DbSet<VendaModel> TabelaVendaModel { get; set; }
 }
