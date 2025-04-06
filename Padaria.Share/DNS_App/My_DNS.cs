2cfc32ebd4ceb1a03c25f7adbb547f281fd9dd4a
CedrickMansoni <cedrick.k.mansoni@gmail.com>
2025-04-06 18:53:11 +0100

    ...

--- Padaria.Share/DNS_App/My_DNS.cs
+++ Padaria.Share/DNS_App/My_DNS.cs
@@ -7,6 +7,6 @@ public static class My_DNS
     public static string App_DNS {get; set;}
     static My_DNS()
     {
-        App_DNS = "https://5b67-129-122-151-81.ngrok-free.app/";    
+        App_DNS = "https://426c-129-122-151-81.ngrok-free.app/";    
     }
 }
