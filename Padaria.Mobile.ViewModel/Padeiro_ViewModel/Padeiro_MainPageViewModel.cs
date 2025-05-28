a70b270138785b1b9f68fa90d23ad62da0c8750e
CedrickMansoni <cedrick.k.mansoni@gmail.com>
2025-05-28 14:16:04 +0100

    ...

--- Padaria.Mobile.ViewModel/Padeiro_ViewModel/Padeiro_MainPageViewModel.cs
+++ Padaria.Mobile.ViewModel/Padeiro_ViewModel/Padeiro_MainPageViewModel.cs
@@ -177,9 +177,11 @@ public class Padeiro_MainPageViewModel : BindableObject
 
     public ICommand ListarProducaoCommand => new Command(async () =>
     {
+        await Shell.Current.DisplayAlert("sms", "Sim 01", "Ok");
         var response = await client.GetAsync($"listar/producao/data/{DateTime.Now.ToString("yyyy-MM-dd")}");
         if (response.IsSuccessStatusCode)
         {
+            await Shell.Current.DisplayAlert("sms", "Sim 02", "Ok");
             using var content = await response.Content.ReadAsStreamAsync();
             ProducaoPedidos = await JsonSerializer.DeserializeAsync<ObservableCollection<Get_Producao_DTO>>(content, options) ?? [];
         }
